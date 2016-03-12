7e748cfe3eeef22ba611159906238bb6d5e445ee
旺仔 <weiwang1202@126.com>
2016-03-12 15:36:32 +0800

    Initial Commit

---
+++ DianJia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DianJia
+//
+//  Created by 魏旺 on 16/3/12.
+//  Copyright © 2016年 olive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
